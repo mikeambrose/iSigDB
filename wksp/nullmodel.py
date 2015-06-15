3921f5836cd096dc18ef60b13fdb9abd74070df3
mikeambrose <mikeambrose@berkeley.edu>
2015-06-15 12:16:28 -0700

    Null model much faster, now uses Chernoff bound for ranks

--- wksp/nullmodel.py
+++ wksp/nullmodel.py
@@ -2,58 +2,50 @@ import random
 import os
 from optparse import OptionParser
 import subprocess
-def shuffleInput(inFile,outFile):
+import matplotlib.pyplot as plt
+from math import e
+def getAllVals(inFile):
     f = open(inFile).read().split('\n')
-    header = f[0]
-    f = f[1:]
-    genes,vals = [],[]
+    samNames = f[0].split('\t')[1:]
+    del f[0]
+    inputs = []
     for line in f:
-        gene,val = line[:line.index('\t')],line[line.index('\t')+1:]
-        genes.append(gene)
-        vals.append(val)
-    with open(outFile,'w') as f:
-        f.write(header+"\n")
-        while genes:
-            i = random.randint(0,len(vals)-1)
-            f.write(genes[0] + "\t" + vals[i] + "\n")
-            del genes[0]
-            del vals[i]
-def getMinMaxPerSig(txtFile):
-    f = open(txtFile).read().split('\n')
-    sigs = f[0].split('\t')[1:]
-    sigDict = {}
-    for sig in sigs:
-        sigDict[sig] = (float('+inf'),float('-inf'))
-    for line in f[1:]:
-        values = line.split('\t')[1:]
-        for i in range(len(values)):
-            sig, value = sigs[i],float(values[i])
-            sigDict[sig] = (min(sigDict[sig][0],value), max(sigDict[sig][1],value))
-    return sigDict
+        inputs.append([float(x) for x in line.split('\t')[1:]])
+    return inputs,samNames
+
+def pickN(vals,n):
+    print len(vals),n
+    subset = random.sample(vals,n)
+    av = []
+    for i in range(len(vals[0])):
+        av.append(sum([sub[i] for sub in subset])/float(n))
+    return av
+
+def nullRank(N,n,x):
+    """probability that the sum of n uniform(1,N) variables is greater than n*x"""
+    t = 1.0/(N+1)
+    #inner = (e - 1) / ((e**t - 1)*(e**(t*x))) / float(N)
+    inner = (e-1)/(N*(e**t-1)*e**(x*t))
+    return inner**n
+
+
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-i", dest="input")
-    parser.add_option("-o", dest="output")
-    parser.add_option("-t", dest="txt")
     parser.add_option("-n", dest="n")
-    parser.add_option("-w", dest="w")
+    parser.add_option("-x", dest="num_iter")
     (options, args) = parser.parse_args()
     sigMinsMaxes = dict()
     FNULL = open(os.devnull,'w')
     import time; t = time.time()
-    for _ in range(int(options.n)):
+    allVals,samNames = getAllVals(options.input)
+    samDists = {}
+    for name in samNames:
+        samDists[name] = []
+    for _ in range(int(options.num_iter)):
         print "iteration",_
-        shuffleInput(options.input,options.output)
-        subprocess.call(["python","/home/mike/workspace/PellegriniResearch/Sig_Avg_Matrix_Derm.Rank.py", "-t",options.output, "-s","/home/mike/workspace/PellegriniResearch/SigGenes.txt", "--group","/home/mike/workspace/PellegriniResearch/SIGS/abbrevs.txt", "-n","50", "-v","rank_avg", "-z","none", "-j","0", "-l", "-r","none", "-c","none", "-i","checked"],stdout=FNULL,stderr=FNULL)
-        sigDict = getMinMaxPerSig(options.txt)
-        for sig in sigDict:
-            if sig in sigMinsMaxes:
-                sigMinsMaxes[sig][0].append(sigDict[sig][0])
-                sigMinsMaxes[sig][1].append(sigDict[sig][1])
-            else:
-                sigMinsMaxes[sig] = ([sigDict[sig][0]],[sigDict[sig][1]])
+        transformedNValues = pickN(allVals,int(options.n))
+        for i in range(len(transformedNValues)):
+            samDists[samNames[i]].append(transformedNValues[i])
     print time.time()-t,"seconds elapsed"
-    with open(options.w,'w') as w:
-        for sig in sigMinsMaxes:
-            w.write(sig + "\t" + str(sigMinsMaxes[sig]) + "\n")
     import pdb;pdb.set_trace()
